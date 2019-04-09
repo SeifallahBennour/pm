4ec997bb596894b10a920565fab1dd60cefac9ce
Ikbel <Administrateur@MOHAMED-PC>
2019-04-09 21:34:52 +0100

    WS2

---
+++ Projectint/WebApplication1/Controllers/PostsWSController.cs
@@ -0,0 +1,69 @@
+﻿using Domain.Entities;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class PostsWSController : ApiController
+    {
+        
+        PostService serviceposts = new PostService();
+
+
+        // GET: api/PostsWS
+        public IEnumerable<Post> Getpost()
+        {
+            return serviceposts.GetAll();
+
+        }
+
+        // GET: api/PostsWS/5
+        public Post Getpost(int id)
+        {
+            return serviceposts.GetById(id);
+        }
+
+        // POST: api/PostsWS
+        //public void Post([FromBody]string value)
+        //{
+        //}
+        PostService p = new PostService();
+        // POST: api/PostsWS
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] Post a)
+        {
+
+            //return p.findByName(a.Name);
+            //return p.findByEmail(a.email);
+
+            try
+            {
+                p.AddPostWS(a);
+
+                var message = Request.CreateErrorResponse(HttpStatusCode.Created, p.ToString());
+                message.Headers.Location = new Uri(Request.RequestUri + p.ToString());
+
+                return message;
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        // PUT: api/PostsWS/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PostsWS/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Projectint/WebApplication1/Controllers/UserWSController.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Entities;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class UserWSController : ApiController
+    {
+       
+        UserService serviceusers = new UserService();
+
+
+        // GET: api/UserWS       
+        public IEnumerable<User> Getuse()
+        {
+            return serviceusers.GetAll();
+
+        }
+
+        // GET: api/UserWS/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/UserWS
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/UserWS/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/UserWS/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
